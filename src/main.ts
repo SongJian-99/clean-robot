d39a845c08ac011ca3cc5400f9c13910c886def1
songjian <1414984960@qq.com>
2024-03-08 17:33:50 +0800

    feat: Vue Router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+/* 引入 Antd 和 样式 */
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+
+import router from '@/router'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Antd)
+app.use(router)
+app.mount('#app')
+
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class=''>
+    hellow
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
